03ba2554eced1449dd8572a92d2b3380ffeff8c0
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-01-20 22:01:43 +0100

    make sure build.rs can work

---
+++ bigtable-rs/build.rs
@@ -0,0 +1,12 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Uncomment the code below and cargo build again, when updating google protos
+
+    // tonic_build::configure()
+    //     .build_server(false)
+    //     .out_dir("src/google")
+    //     .compile(
+    //         &["../googleapis/google/bigtable/v2/bigtable.proto"],
+    //         &["../googleapis"],
+    //     )?;
+    Ok(())
+}
