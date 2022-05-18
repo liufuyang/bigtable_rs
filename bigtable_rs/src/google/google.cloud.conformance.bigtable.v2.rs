6fc6c57bf965a962fdbdf7bb1eb4e3e84f6d21c3
Fuyang Liu <liufuyang@users.noreply.github.com>
2022-05-18 12:16:49 +0200

    Update prost and generated source

--- bigtable_rs/src/google/google.cloud.conformance.bigtable.v2.rs
+++ bigtable_rs/src/google/google.cloud.conformance.bigtable.v2.rs
@@ -1,17 +1,15 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct TestFile {
-    #[prost(message, repeated, tag = "1")]
+    #[prost(message, repeated, tag="1")]
     pub read_rows_tests: ::prost::alloc::vec::Vec<ReadRowsTest>,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ReadRowsTest {
-    #[prost(string, tag = "1")]
+    #[prost(string, tag="1")]
     pub description: ::prost::alloc::string::String,
-    #[prost(message, repeated, tag = "2")]
-    pub chunks: ::prost::alloc::vec::Vec<
-        super::super::super::super::bigtable::v2::read_rows_response::CellChunk,
-    >,
-    #[prost(message, repeated, tag = "3")]
+    #[prost(message, repeated, tag="2")]
+    pub chunks: ::prost::alloc::vec::Vec<super::super::super::super::bigtable::v2::read_rows_response::CellChunk>,
+    #[prost(message, repeated, tag="3")]
     pub results: ::prost::alloc::vec::Vec<read_rows_test::Result>,
 }
 /// Nested message and enum types in `ReadRowsTest`.
@@ -20,19 +18,19 @@ pub mod read_rows_test {
     /// Only the last result can be an error.
     #[derive(Clone, PartialEq, ::prost::Message)]
     pub struct Result {
-        #[prost(string, tag = "1")]
+        #[prost(string, tag="1")]
         pub row_key: ::prost::alloc::string::String,
-        #[prost(string, tag = "2")]
+        #[prost(string, tag="2")]
         pub family_name: ::prost::alloc::string::String,
-        #[prost(string, tag = "3")]
+        #[prost(string, tag="3")]
         pub qualifier: ::prost::alloc::string::String,
-        #[prost(int64, tag = "4")]
+        #[prost(int64, tag="4")]
         pub timestamp_micros: i64,
-        #[prost(string, tag = "5")]
+        #[prost(string, tag="5")]
         pub value: ::prost::alloc::string::String,
-        #[prost(string, tag = "6")]
+        #[prost(string, tag="6")]
         pub label: ::prost::alloc::string::String,
-        #[prost(bool, tag = "7")]
+        #[prost(bool, tag="7")]
         pub error: bool,
     }
 }
